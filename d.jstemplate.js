d4b5f47fbe1cdc92f7c6594c23b5f96373ef3649
ohts1031ECW <97420394+ohts1031ECW@users.noreply.github.com>
2022-06-19 17:13:55 +0900

    Add files via upload

---
+++ d.jstemplate.js
@@ -0,0 +1,14 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents :[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('MessageCreate', async (message) => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+})
+
+client.login("your token here");
